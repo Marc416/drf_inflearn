1c623192025313150e5c36c1f889841bf28a3732
Marc416 <juni416@naver.com>
2022-05-07 23:21:45 +0900

    테이블을 만들고 Viewset으로  확인해보고 apiview로 더 세밀한 api를 만들것 처음 api를 만들데 제약 없이 만들기에 적절함.

--- api2/views.py
+++ api2/views.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 
 from api2.serializers import UserSerializer
+from api2.serializers import PostSerializer
+from blog.models import Post
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
