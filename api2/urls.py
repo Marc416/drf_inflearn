a37f9372427b3dc47d74398c487fcb6d5aa5fc4e
Marc416 <juni416@naver.com>
2022-05-08 01:46:13 +0900

    Change Vieset to APIView

--- api2/urls.py
+++ api2/urls.py
@@ -1,17 +1,29 @@
 # Routers provide an easy way of automatically determining the URL conf.
+# from django.urls import path, include
+# from rest_framework import routers
+
+# from api2.views import UserViewSet
+# from api2.views import PostViewSet
+# from api2.views import CommentViewSet
+
+# # ViewSet을 사용할때만 라우터를 사용한다
+# router = routers.DefaultRouter()
+# router.register(r'user', UserViewSet)
+# router.register(r'post', PostViewSet)
+# router.register(r'comment', CommentViewSet)
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+# GenericAPIView 활용하기
 from django.urls import path, include
-from rest_framework import routers
 
-from api2.views import UserViewSet
-from api2.views import PostViewSet
-from api2.views import CommentViewSet
-
-
-router = routers.DefaultRouter()
-router.register(r'user', UserViewSet)
-router.register(r'post', PostViewSet)
-router.register(r'comment', CommentViewSet)
+from api2 import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # name을 drf명명법을 따라 표기
+    path('post', include(views.PostListAPIView.as_view(), names='post-list')),
+    path('post/<int:pk>', include(views.PostRetrieveAPIView.as_view(), names='post-detail')),
+    path('comment', include(views.CommentCreateAPIView.as_view(), names='comment-list')),
 ]
--- api2/views.py
+++ api2/views.py
@@ -1,23 +1,47 @@
-# ViewSets define the view behavior.
-from django.contrib.auth.models import User
-from rest_framework import viewsets
+# # ViewSets define the view behavior.
+# from django.contrib.auth.models import User
+# from rest_framework import viewsets
+# from rest_framework.generics import ListAPIView
+#
+# from api2.serializers import UserSerializer
+# from api2.serializers import PostSerializer
+# from api2.serializers import CommentSerializer
+# from blog.models import Post, Comment
+#
+#
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class PostViewSet(viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class CommentViewSet(viewsets.ModelViewSet):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+# --------------------------------------------------
 
-from api2.serializers import UserSerializer
-from api2.serializers import PostSerializer
-from api2.serializers import CommentSerializer
+# GenericApiView 로 만들어보기
+# api2/post[GET], api2/post/pk[GET], api2/comment[POST] 를 변경해보기
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+
+from api2.serializers import PostSerializer, CommentSerializer
 from blog.models import Post, Comment
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class PostViewSet(viewsets.ModelViewSet):
+class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class CommentViewSet(viewsets.ModelViewSet):
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
