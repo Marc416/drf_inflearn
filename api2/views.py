ba259890a242f3814bb60250f25e58c9fa193d45
Marc416 <juni416@naver.com>
2022-05-11 22:53:03 +0900

    PATCH vs GET ; If user do not input data, use GET

--- api2/views.py
+++ api2/views.py
@@ -26,7 +26,7 @@
 
 # GenericApiView 로 만들어보기
 # api2/post[GET], api2/post/pk[GET], api2/comment[POST] 를 변경해보기
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, RetrieveUpdateAPIView, GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -44,30 +44,42 @@ class PostRetrieveAPIView(RetrieveAPIView):
     serializer_class = PostRetrieveSerializer
 
 
-class PostLikeAPIView(UpdateAPIView):
+# class PostLikeAPIView(UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostLikeSerializer
+#
+#     # Put 과 Patch의 차이점:
+#     # Blank true가 아닌 필드는 필수 필드이고 필수 필드는 Put 메서드실행시 필요.
+#     # Patch에서는 필수필드가 필요없다.
+#     # 장고쉘에서 시리얼 라이저 필드중 필수 필드를 확인할 수 있음
+#     # PATCH
+#     def update(self, request, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         instance = self.get_object()
+#         # 변경하고자 하는 필드만 변경
+#         data = {'like': instance.like + 1}
+#         serializer = self.get_serializer(instance, data=data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_update(serializer)
+#
+#         if getattr(instance, '_prefetched_objects_cache', None):
+#             # If 'prefetch_related' has been applied to a queryset, we need to
+#             # forcibly invalidate the prefetch cache on the instance.
+#             instance._prefetched_objects_cache = {}
+#
+#         return Response(data['like'])
+
+# PATCH vs GET
+# like 는 사용자의 데이터 입력 부분없이 이벤트에 의해 작동되므로 GET 으로 동작해도 됨
+class PostLikeAPIView(GenericAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostLikeSerializer
 
-    # Put 과 Patch의 차이점:
-    # Blank true가 아닌 필드는 필수 필드이고 필수 필드는 Put 메서드실행시 필요.
-    # Patch에서는 필수필드가 필요없다.
-    # 장고쉘에서 시리얼 라이저 필드중 필수 필드를 확인할 수 있음
-    # PATCH
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+    def get(self, request, *args, **kwargs):
         instance = self.get_object()
-        # 변경하고자 하는 필드만 변경
-        data = {'like': instance.like + 1}
-        serializer = self.get_serializer(instance, data=data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        instance.like += 1
+        instance.save()
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(data['like'])
+        return Response(instance.like)
 
 
 class CommentCreateAPIView(CreateAPIView):
