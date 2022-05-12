9d7e5d32b7791ba3c77fbb6af4515b713c172ab2
Marc416 <juni416@naver.com>
2022-05-13 00:13:18 +0900

    Make Api by Viewset(Practice)

--- api2/urls.py
+++ api2/urls.py
@@ -25,9 +25,27 @@ urlpatterns = [
     # name을 drf명명법을 따라 표기
     # defaultrouter를 사용시 root url인 'api2/' 가 만들어짐. 하지만 GenericView를 이용했기 때문에
     # 루트라 라우터는 만들어지지 않음
-    path('post/', views.PostListAPIView.as_view(), name='post-list'),
-    path('post/<int:pk>/', views.PostRetrieveAPIView.as_view(), name='post-detail'),
-    path('post/<int:pk>/like/', views.PostLikeAPIView.as_view(), name='post-like'),
-    path('comment/', views.CommentCreateAPIView.as_view(), name='comment-list'),
+    # path('post/', views.PostListAPIView.as_view(), name='post-list'),
+    # path('post/<int:pk>/', views.PostRetrieveAPIView.as_view(), name='post-detail'),
+    # path('post/<int:pk>/like/', views.PostLikeAPIView.as_view(), name='post-like'),
+    # path('comment/', views.CommentCreateAPIView.as_view(), name='comment-list'),
+    # path('catetag/', views.CateTagListAPIView.as_view(), name='catetag_list'),
+
+    path('post/', views.PostViewSet.as_view(actions={
+        # get 요청이오면 list 메서드로 바인딩 ; list는 ViewSet 기본 메서드
+        'get': 'list'
+    }), name='post-list'),
+    path('post/<int:pk>/', views.PostViewSet.as_view(actions={
+        # get 요청이오면 retrieve 메서드로 바인딩 ; retrieve ViewSet 기본 메서드
+        'get': 'retrieve'
+    }), name='post-detail'),
+    path('post/<int:pk>/like/', views.PostViewSet.as_view(actions={
+        'get': 'like'
+    }), name='post-like'),
+
+    path('comment/', views.CommentViewSet.as_view(actions={
+        'post': 'create'
+    }), name='comment-list'),
+
     path('catetag/', views.CateTagListAPIView.as_view(), name='catetag_list'),
 ]
--- api2/views.py
+++ api2/views.py
@@ -26,111 +26,36 @@
 
 # GenericApiView 로 만들어보기
 # api2/post[GET], api2/post/pk[GET], api2/comment[POST] 를 변경해보기
+from collections import OrderedDict
+
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, RetrieveUpdateAPIView, GenericAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from api2.serializers import PostListSerializer, CommentSerializer, PostRetrieveSerializer, PostLikeSerializer, CateTagSerializer, PostSerializerDetail
 from blog.models import Post, Comment, Category, Tag
 
 
-class PostListAPIView(ListAPIView):
+class PostPageNumberPagination(PageNumberPagination):
+    page_size = 3
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('postList', data),
+            ('pageCnt', self.page.paginator.num_pages),
+            ('curPage', self.page.number),
+        ]))
+
+
+class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    pagination_class = PostPageNumberPagination
 
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        return {
-            # 상속에 속성에 따라 request를 None 처리하면 Url이 상대 url로 나오게됨
-            'request': None,
-            'format': self.format_kwarg,
-            'view': self
-        }
-
-
-# class PostRetrieveAPIView(RetrieveAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostRetrieveSerializer
-
-def get_prev_next(instance):
-    try:
-        prev = instance.get_previous_by_update_dt()
-    except instance.DoesNotExist:
-        # exception은 인스턴스나 모델에 붙여서 사용
-        prev = None
-
-    try:
-        next_ = instance.get_next_by_update_dt()
-    except instance.DoesNotExist:
-        next_ = None
-    return prev, next_
-
-
-class PostRetrieveAPIView(RetrieveAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializerDetail
-
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        return {
-            # 상속에 속성에 따라 request를 None 처리하면 Url이 상대 url로 나오게됨
-            'request': None,
-            'format': self.format_kwarg,
-            'view': self
-        }
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        # 장고에서 기본으로 제공해주는 인스턴 : 해당인스턴스 앞뒤를 부름
-        prevInstance, nextInstance = get_prev_next(instance=instance)
-        commentList = instance.comment_set.all()
-        data = {
-            'post': instance,
-            'prevPost': prevInstance,
-            'nextPost': nextInstance,
-            'commentList': commentList,
-
-        }
-        serializer = self.get_serializer(instance=data)
-        # 직렬화는 serializer.data 할때 직렬화 진행됨
-        return Response(serializer.data)
-
-
-# class PostLikeAPIView(UpdateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostLikeSerializer
-#
-#     # Put 과 Patch의 차이점:
-#     # Blank true가 아닌 필드는 필수 필드이고 필수 필드는 Put 메서드실행시 필요.
-#     # Patch에서는 필수필드가 필요없다.
-#     # 장고쉘에서 시리얼 라이저 필드중 필수 필드를 확인할 수 있음
-#     # PATCH
-#     def update(self, request, *args, **kwargs):
-#         partial = kwargs.pop('partial', False)
-#         instance = self.get_object()
-#         # 변경하고자 하는 필드만 변경
-#         data = {'like': instance.like + 1}
-#         serializer = self.get_serializer(instance, data=data, partial=partial)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_update(serializer)
-#
-#         if getattr(instance, '_prefetched_objects_cache', None):
-#             # If 'prefetch_related' has been applied to a queryset, we need to
-#             # forcibly invalidate the prefetch cache on the instance.
-#             instance._prefetched_objects_cache = {}
-#
-#         return Response(data['like'])
-
-# PATCH vs GET
-# like 는 사용자의 데이터 입력 부분없이 이벤트에 의해 작동되므로 GET 으로 동작해도 됨
-class PostLikeAPIView(GenericAPIView):
-    queryset = Post.objects.all()
-
-    def get(self, request, *args, **kwargs):
+    # Viewset에서는 get 메서드를 사용하지 않으므로 다른 메서드로 바구고 url 매핑해주기
+    def like(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.like += 1
         instance.save()
@@ -138,9 +63,72 @@ class PostLikeAPIView(GenericAPIView):
         return Response(instance.like)
 
 
-class CommentCreateAPIView(CreateAPIView):
+# class PostListAPIView(ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostListSerializer
+#
+#     def get_serializer_context(self):
+#         """
+#         Extra context provided to the serializer class.
+#         """
+#         return {
+#             # 상속에 속성에 따라 request를 None 처리하면 Url이 상대 url로 나오게됨
+#             'request': None,
+#             'format': self.format_kwarg,
+#             'view': self
+#         }
+#
+
+# class PostRetrieveAPIView(RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializerDetail
+#
+#     def get_serializer_context(self):
+#         """
+#         Extra context provided to the serializer class.
+#         """
+#         return {
+#             # 상속에 속성에 따라 request를 None 처리하면 Url이 상대 url로 나오게됨
+#             'request': None,
+#             'format': self.format_kwarg,
+#             'view': self
+#         }
+#
+#     def retrieve(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         # 장고에서 기본으로 제공해주는 인스턴 : 해당인스턴스 앞뒤를 부름
+#         prevInstance, nextInstance = get_prev_next(instance=instance)
+#         commentList = instance.comment_set.all()
+#         data = {
+#             'post': instance,
+#             'prevPost': prevInstance,
+#             'nextPost': nextInstance,
+#             'commentList': commentList,
+#
+#         }
+#         serializer = self.get_serializer(instance=data)
+#         # 직렬화는 serializer.data 할때 직렬화 진행됨
+#         return Response(serializer.data)
+#
+#
+# class PostLikeAPIView(GenericAPIView):
+#     queryset = Post.objects.all()
+#
+#     def get(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         instance.like += 1
+#         instance.save()
+#
+#         return Response(instance.like)
+
+class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+# class CommentCreateAPIView(CreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
 
 
 class CateTagListAPIView(APIView):
